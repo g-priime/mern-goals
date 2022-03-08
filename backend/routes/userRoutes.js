caa5209ea41888e7d47eb5f3841268abb575d26f
g-priime <57049481+g-priime@users.noreply.github.com>
2022-03-08 16:57:20 -0700

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const { registerUser } = require('../controllers/userController')
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
 
-router.post('/', registerUser)
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", getMe);
 
-module.exports = router
+module.exports = router;
