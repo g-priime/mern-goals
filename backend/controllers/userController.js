2e4a1d51b608cd0e8cb152995503baeee2fb1788
g-priime <57049481+g-priime@users.noreply.github.com>
2022-03-08 18:03:19 -0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -72,7 +72,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   Get /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data display" });
+  const { _id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
 
 // Generate JWT
