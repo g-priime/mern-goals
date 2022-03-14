0e642beb986a84b1146f818c04086148aea10755
g-priime <57049481+g-priime@users.noreply.github.com>
2022-03-14 14:19:40 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ connectDB();
 
 const app = express();
 
+/*
 const cors = require("cors");
 const whitelist = ["http://localhost:3000"];
 const corsOptions = {
@@ -23,6 +24,7 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
+*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
