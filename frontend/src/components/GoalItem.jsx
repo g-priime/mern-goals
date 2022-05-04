5e9e0dbde991f7a6cc1363378382ab3750c499fc
g-priime <57049481+g-priime@users.noreply.github.com>
2022-05-04 14:45:53 -0600

    get goal image data

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,12 +1,22 @@
 import { useDispatch } from "react-redux";
 import { deleteGoal } from "../features/goals/goalSlice";
 
+//const imgUrl = "data:image/jpeg;base64," + BinaryToBase64(goal.img);
+
 function GoalItem({ goal }) {
   const dispatch = useDispatch();
 
   return (
     <div className="goal">
       <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
+      <div>
+        {goal.img.data.data.length > 0 ? (
+          <img src={`data:image/jpeg;base64,${goal.img.data.data}`} alt="" />
+        ) : (
+          <>No image</>
+        )}
+      </div>
+      <div>{goal.img.data.type}</div>
       <h2>{goal.text}</h2>
       <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
         X
